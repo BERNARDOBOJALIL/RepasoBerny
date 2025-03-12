e280eef9968b299a20c2910eae15eca0782313ea
BERNARDOBOJALIL <bernardo.bojalil.lorenzini@gmail.com>
2025-03-12 14:58:25 -0600

    Avance MVVM

--- RepasoBerny.Mobile/MainPage.xaml.cs
+++ RepasoBerny.Mobile/MainPage.xaml.cs
@@ -9,19 +9,7 @@
             InitializeComponent();
         }
 
-        private async void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-            await Navigation.PushModalAsync(new Pages.TestPage());
-            //await Navigation.PopAsync();
-        }
+        
     }
 
 }
---
+++ RepasoBerny.Mobile/ViewModel/ViewModelTest.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoBerny.Mobile.ViewModel
+{
+    partial class ViewModelTest:ObservableObject
+    {
+        [ObservableProperty]
+        public string text;
+        [ObservableProperty]
+        int count;
+        [RelayCommand]
+        public void CambiarTexto()
+        {
+            Count++;
+            Text = "Hola Mundo";
+        }
+    }
+}
